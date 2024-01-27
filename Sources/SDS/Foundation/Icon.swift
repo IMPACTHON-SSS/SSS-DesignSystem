b0214a0ffd616a72b2e209bff54b16bd71467a14
Mercen <mercen@mercen.net>
2024-01-27 15:33:18 +0900

    ADD: More Icons

--- Sources/SDS/Foundation/Icon.swift
+++ Sources/SDS/Foundation/Icon.swift
@@ -3,5 +3,9 @@ import SwiftUI
 @available(iOS 15, macOS 12, *)
 public extension Image {
     
-    static let check: Image = Image("check", bundle: .module)
+    static let check   = Image("Check", bundle: .module)
+    static let home    = Image("Home", bundle: .module)
+    static let my      = Image("My", bundle: .module)
+    static let pay     = Image("Pay", bundle: .module)
+    static let receipt = Image("Receipt", bundle: .module)
 }
