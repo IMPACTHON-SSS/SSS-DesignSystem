e71318e60063790b7872b90f13fecbf34e175095
Mercen <mercen@mercen.net>
2024-01-27 15:00:53 +0900

    Initial Commit

---
+++ Tests/SSS-DesignSystemTests/SSS_DesignSystemTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import SSS_DesignSystem
+
+final class SSS_DesignSystemTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
