43d1925597f099e7a9b67ee45a951eb9d1f05e8b
Mercen <mercen@mercen.net>
2024-01-27 15:37:05 +0900

    UPDATE: Icons

---
+++ Sources/SDS/Foundation/Image.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+@available(iOS 15, macOS 12, *)
+public extension Image {
+    
+    static let add            = Image("Check", bundle: .module)
+    static let bigArrow       = Image("Home", bundle: .module)
+    static let check          = Image("My", bundle: .module)
+    static let creditCard     = Image("Pay", bundle: .module)
+    static let dot            = Image("Dot", bundle: .module)
+    static let home           = Image("Home", bundle: .module)
+    static let menu           = Image("Menu", bundle: .module)
+    static let notification   = Image("Notification", bundle: .module)
+    static let notificationOn = Image("NotificationOn", bundle: .module)
+    static let person         = Image("Person", bundle: .module)
+    static let receipt        = Image("Receipt", bundle: .module)
+    static let reset          = Image("Reset", bundle: .module)
+    static let search         = Image("Search", bundle: .module)
+    static let smallArrow     = Image("SmallArrow", bundle: .module)
+}
